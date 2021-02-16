e8881ddf249db8cc0048bd511281ebe43ad88d6e
davidbragat <davidbragat@gmail.com>
2021-02-15 22:17:18 -0300

    added html files

--- exercicios-web/fundamentos/objeto2.js
+++ exercicios-web/fundamentos/objeto2.js
@@ -8,3 +8,14 @@ console.log(typeof new Cliente)
 class Produto {} // ES 2015 (ES6)
 console.log(typeof Produto)
 console.log(typeof new Produto())
+
+class Notas{
+    nota1: 10,
+    nota2: 5.5,
+    nota3: 7,
+    nota4:8,
+}
+
+let provaFinal = new Notas;
+
+console.log(provaFinal);
